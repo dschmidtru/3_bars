707f197163ec31dc68dd79661c157170877cbded
Xaocadv <Xaocadv@gmail.com>
2017-05-24 15:55:38 +0300

    Исправлены замечания от 18.05.2015
    1. Изменен README.md
    2. Местами изменены комментарии
    3. Убрано закрытие файлы в конструкции with
    4. Считываются все строки из файла Бары.json
    5. подправлена функция считывания json-файл
    6. Переделан поиск больших и маленьких баров
    7. Изменена функция расчета растояния (считаем что считаем на плоскости), вместо поиска расстояния на сфере.
    8. Изменены названия переменных и функций
    9. Изменен код считывания ввода пользователя

--- bars.py
+++ bars.py
@@ -3,229 +3,182 @@ import sys
 import math
 
 
-def load_data(filepath):
-    """
-    Read json from file
-    :param filepath: Filepath of data file
-    :return: data from file as list
-    """
-    with open(filepath, 'r', encoding="utf8") as input_data_file:
-        json_string = input_data_file.readline()
-    input_data_file.close()
+def load_data(file_path):
+    """ read json data from file and return as list """
+    json_object = []
+    with open(file_path, 'r', encoding="utf8") as input_data_file:
+        json_string = input_data_file.readlines()
     try:
-        json_data = json.loads(json_string)
-        return json_data
+        for i in range(len(json_string)):
+            json_object.append(json.loads(json_string[i]))
     except ValueError:
         print("cann't convert json data from file")
-        exit()
+        return False
+    return json_object
 
 
-def get_biggest_bar(data):
-    biggest_bar = {'Address': '', 'AdmArea': '', 'District': '', 'Name': '',
-                   'SeatsCount': 0}
-    bar_itog_info = []
-    for cur_bar_data in data:
-        cur_bar_data = cur_bar_data['Cells']
-        if cur_bar_data['SeatsCount'] == biggest_bar['SeatsCount']:
-            biggest_bar['Address'] = cur_bar_data['Address']
-            biggest_bar['AdmArea'] = cur_bar_data['AdmArea']
-            biggest_bar['District'] = cur_bar_data['District']
-            biggest_bar['Name'] = cur_bar_data['Name']
-            biggest_bar['SeatsCount'] = cur_bar_data['SeatsCount']
-            bar_itog_info.append(biggest_bar)
-        if cur_bar_data['SeatsCount'] > biggest_bar['SeatsCount']:
-            bar_itog_info = []
-            biggest_bar['Address'] = cur_bar_data['Address']
-            biggest_bar['AdmArea'] = cur_bar_data['AdmArea']
-            biggest_bar['District'] = cur_bar_data['District']
-            biggest_bar['Name'] = cur_bar_data['Name']
-            biggest_bar['SeatsCount'] = cur_bar_data['SeatsCount']
-            bar_itog_info.append(biggest_bar)
-    return bar_itog_info
-
-
-def get_smallest_bar(data):
-    smallest_bar = {'Address': '', 'AdmArea': '', 'District': '', 'Name': '',
-                    'SeatsCount': sys.maxsize}
-    bar_itog_info = []
-    for cur_bar_data in data:
-        cur_bar_data = cur_bar_data['Cells']
-        if cur_bar_data['SeatsCount'] == smallest_bar['SeatsCount']:
-            smallest_bar = {'Address': cur_bar_data['Address'],
-                            'AdmArea': cur_bar_data['AdmArea'],
-                            'District': cur_bar_data['District'],
-                            'Name': cur_bar_data['Name'],
-                            'SeatsCount': cur_bar_data['SeatsCount']}
-            bar_itog_info.append(smallest_bar)
-        if cur_bar_data['SeatsCount'] < smallest_bar['SeatsCount']:
-            bar_itog_info = []
-            smallest_bar['Address'] = cur_bar_data['Address']
-            smallest_bar['AdmArea'] = cur_bar_data['AdmArea']
-            smallest_bar['District'] = cur_bar_data['District']
-            smallest_bar['Name'] = cur_bar_data['Name']
-            smallest_bar['SeatsCount'] = cur_bar_data['SeatsCount']
-            bar_itog_info.append(smallest_bar)
-    return bar_itog_info
-
-
-def calc_distance(f_a, l_a, f_b, l_b):
+def min_max(function_name, list_data):
     """
-    :param f_a: longitude of A point
-    :param l_a: latitude of A point
-    :param f_b: longitude of B point
-    :param l_b: latitude of B point
-    :return: distance bitween two point on map
-    description of algorithm can be found here:
-    http://gis-lab.info/qa/great-circles.html
+    :param function_name: we determine what we whant ti find min or max
+    :param list_data: list in which we search min or max
+    :return: min or max element and positin of this element in list
     """
-
-    earth_radius = 6372795
-
-    lat1 = f_a * math.pi / 180
-    lat2 = f_b * math.pi / 180
-    long1 = l_a * math.pi / 180
-    long2 = l_b * math.pi / 180
-
-    cl1 = math.cos(lat1)
-    cl2 = math.cos(lat2)
-    sl1 = math.sin(lat1)
-    sl2 = math.sin(lat2)
-    delta = long2 - long1
-    cdelta = math.cos(delta)
-    sdelta = math.sin(delta)
-
-    y = math.sqrt(math.pow(cl2 * sdelta, 2) + math.pow(cl1 * sl2 - sl1 *
-                                                       cl2 * cdelta, 2))
-    x = sl1 * sl2 + cl1 * cl2 * cdelta
-
-    ad = math.atan2(y, x)
-    dist = ad * earth_radius
-
-    return dist
-
-
-def get_closest_bar(data, longitude, latitude):
-    closest_bar = {'Address': '', 'AdmArea': '', 'District': '', 'Name': '',
-                   'SeatsCount': 0, 'longitude': 0, 'latitude': 0}
-    bar_itog_info = []
-    num = 0
-    min_dist = 0
-    while num < (len(data)) - 1:
-        if num == 0:
-            info = data[num]['Cells']
-            closest_bar = {'Address': info['Address'],
-                           'AdmArea': info['AdmArea'],
-                           'District': info['District'],
-                           'Name': info['Name'],
-                           'SeatsCount': info['SeatsCount'],
-                           'longitude': info['geoData']['coordinates'][0],
-                           'latitude': info['geoData']['coordinates'][1]}
-            min_dist = calc_distance(longitude, latitude,
-                                     closest_bar['longitude'],
-                                     closest_bar['latitude'])
-            closest_bar['distance'] = min_dist
-            bar_itog_info.append(closest_bar)
-        else:
-            info = data[num]['Cells']
-            dist = calc_distance(longitude, latitude,
-                                 info['geoData']['coordinates'][0],
-                                 info['geoData']['coordinates'][1])
-            if dist == min_dist:
-                info = data[num]['Cells']
-                closest_bar = {'Address': info['Address'],
-                               'AdmArea': info['AdmArea'],
-                               'District': info['District'],
-                               'Name': info['Name'],
-                               'SeatsCount': info['SeatsCount'],
-                               'longitude': info['geoData']['coordinates'][0],
-                               'latitude': info['geoData']['coordinates'][1],
-                               'distance': dist}
-                bar_itog_info.append(closest_bar)
-            elif dist < min_dist:
-                bar_itog_info = []
-                info = data[num]['Cells']
-                closest_bar['Address'] = info['Address']
-                closest_bar['AdmArea'] = info['AdmArea']
-                closest_bar['District'] = info['District']
-                closest_bar['Name'] = info['Name']
-                closest_bar['SeatsCount'] = info['SeatsCount']
-                closest_bar['longitude'] = info['geoData']['coordinates'][0]
-                closest_bar['latitude'] = info['geoData']['coordinates'][1]
-                closest_bar['distance'] = dist
-                bar_itog_info.append(closest_bar)
-        num = num + 1
-    return bar_itog_info
-
-
-def printing_data(info_str, data):
-    print(info_str)
-    if len(data) > 1:
-        for elem in data:
-            if 'distance' in data[0]:
-                strp = "Ближайший бар на расстоянии %.2f метров. " \
-                       "Название бара: %s расположен по адресу %s %s %s." \
-                       "Имеет %d seats посадочных мест\n" % \
-                       (elem['distance'], elem['Name'], elem['Address'],
-                        elem['AdmArea'], elem['District'], elem['SeatsCount'])
-                print(strp)
-            else:
-                strp = "Название бара: %s расположен по адресу %s %s %s." \
-                       "Имеет %d seats посадочных мест\n" % \
-                       (elem['Name'], elem['Address'], elem['AdmArea'],
-                        elem['District'], elem['SeatsCount'])
-                print(strp)
+    if function_name == "min":
+        m = min(list_data)
+        minpos = [i + 1 for i, j in enumerate(list_data) if j == m]
+        return minpos
+    elif function_name == "max":
+        m = max(list_data)
+        maxpos = [i + 1 for i, j in enumerate(list_data) if j == m]
+        return maxpos
     else:
-        print('')
-        if 'distance' in data[0]:
-            strp = "Ближайший бар на расстоянии %.2f метров. " \
-                   "Название бара: %s расположен по адресу %s %s %s." \
-                   "Имеет %d seats посадочных мест\n" % \
-                   (data[0]['distance'], data[0]['Name'], data[0]['Address'],
-                    data[0]['AdmArea'], data[0]['District'],
-                    data[0]['SeatsCount'])
-            print(strp)
+        return None
 
-        else:
-            strp = "Название бара: %s расположен по адресу %s %s %s." \
-                   " Имеет %d seats посадочных мест\n" % \
-                   (data[0]['Name'], data[0]['Address'], data[0]['AdmArea'],
-                    data[0]['District'], data[0]['SeatsCount'])
-            print(strp)
+
+def get_seats_number(data):
+    """
+    data - list from json
+    list[0] set as "None", because the first number of Bar in json equal 1
+    return list as list[k] = v, where  k - Number of bar in json
+                                    v - number of seats in bar
+    """
+    seats = []
+    for elem in data:
+        seats.append(elem["Cells"]["SeatsCount"])
+    return seats
 
 
 def user_input(desc_string):
-    """
-    Validation of data entry
-    :param desc_string:  the string describe the input
-    :return: latitude or longitude as float
-    """
-    check_input = True
-    input_data = ""
-    while check_input:
-        input_data = input(desc_string)
-        try:
-            input_data = float(input_data)
-            check_input = False
-        except ValueError:
-            print("You must specify coordinate")
+    """ Validation of data entry.
+    desc_string:  the string describe the input for user
+    return: latitude or longitude as float """
+    input_data = input(desc_string)
+    try:
+        input_data = float(input_data)
+    except ValueError:
+        print("You must specify coordinate")
+        print("Script stop work ...")
     return input_data
 
 
-if __name__ == '__main__':
-    bars_data = ""
+def get_closest_bar(data, longitude, latitude):
+    """ find nearest bar
+    :param data: list with info about bars
+    :param longitude: coordinate
+    :param latitude:  coordinate
+    :return: info about nearest bar
+    """
+    bar_itog_info = []
+    list_pos = 0
+    min_dist = sys.maxsize
+    while list_pos < len(data) - 1:
+        info = data[list_pos]['Cells']
+        cur_dist = calc_distance(longitude,
+                                 latitude,
+                                 info['geoData']['coordinates'][0],
+                                 info['geoData']['coordinates'][1])
+        if cur_dist == min_dist:
+            bar_itog_info.append(data[list_pos]['Number'])
+        elif cur_dist < min_dist:
+            bar_itog_info = list()
+            bar_itog_info.append(data[list_pos]['Number'])
+            min_dist = cur_dist
+        list_pos += 1
+    return bar_itog_info
+
+
+def string_info_about_bar(bar_name, bar_address, bar_adm_area, bar_district,
+                          bar_seats_count, bar_dsitance=None):
+    """
+    return sting info about bar. If distance determine, return
+    info string with distance for nearest bar
+    """
+    if bar_dsitance:
+        str_bar_info = "Ближайший бар на расстоянии %.2f метров. " \
+                       "Название бара: %s расположен по адресу %s %s %s." \
+                       "Имеет %d seats посадочных мест\n" % \
+                       (bar_dsitance, bar_name, bar_address,
+                        bar_adm_area, bar_district, bar_seats_count)
+    else:
+        str_bar_info = "Название бара: %s расположен по адресу %s %s %s." \
+                       "Имеет %d seats посадочных мест\n" % \
+                       (bar_name, bar_address, bar_adm_area,
+                        bar_district, bar_seats_count)
+    return str_bar_info
+
+
+def print_data(info_str, bar_numbers, json_string):
+    """
+    print info about bar
+    :param info_str: small info about printing data
+    :param bar_numbers: certian bars number in json
+    :param json_string: json with info about bars
+    :return: none
+    """
+    print(info_str)
+    for i in range(len(bar_numbers)):
+        elem = json_string[bar_numbers[i] - 1]['Cells']
+        if info_str != "Близкие бары:":
+            str_info_bar = string_info_about_bar(elem['Name'],
+                                                 elem['Address'],
+                                                 elem['AdmArea'],
+                                                 elem['District'],
+                                                 elem['SeatsCount'])
+        else:
+            dist = calc_distance(input_longitude,
+                                 input_latitude,
+                                 elem['geoData']['coordinates'][0],
+                                 elem['geoData']['coordinates'][1])
+            str_info_bar = string_info_about_bar(elem['Name'],
+                                                 elem['Address'],
+                                                 elem['AdmArea'],
+                                                 elem['District'],
+                                                 elem['SeatsCount'],
+                                                 dist)
+        print(str_info_bar)
+
+
+def calc_distance(f_a, l_a, f_b, l_b):
+    """calculate distance between to points"""
+    # calculate how meters in 1 deg latitude
+    urm = 6371000 * 2 * math.pi / 360
+    # calculate how meters in 1 deg longitude
+    urp = math.cos(l_a) * urm
+    dist = math.sqrt(
+        math.pow((f_b - f_a) * urp, 2) + math.pow((l_b - l_a) * urm, 2))
+    return dist
+
+
+def main_func(json_data, inp_latitude, inp_longitude):
+    bars_seats_numbmer = get_seats_number(json_data)
+    minpos = min_max("min", bars_seats_numbmer)
+    maxpos = min_max("max", bars_seats_numbmer)
+    closest_bars = get_closest_bar(json_data, inp_longitude, inp_latitude)
+
+    print_data("Большие бары:", maxpos, json_data)
+    print('\n')
+    print_data("Маленькие бары:", minpos, json_data)
+    print('\n')
+    print_data('Близкие бары:', closest_bars, json_data)
+    print('\n')
+
+
+if __name__ == "__main__":
+    # if my_environment = "test" we use predefine coordinates
+    my_environment = "prod"
+    bars_data = list()
     if len(sys.argv) > 1:
         bars_data = load_data(sys.argv[1])
     else:
         print("Please, enter the filepath of data file")
         exit()
-    input_latitude = user_input('Please, enter latitude:\n')
-    input_longitude = user_input('Please, enter longitude:\n')
+    if my_environment != "test":
+        input_latitude = user_input('Please, enter latitude:\n')
+        input_longitude = user_input('Please, enter longitude:\n')
+    else:
+        input_latitude = 55.751984
+        input_longitude = 37.621128
     print('\n')
-    biggest_bars = get_biggest_bar(bars_data)
-    printing_data('Большие бары:', biggest_bars)
-    print('\n')
-    smallest_bars = get_smallest_bar(bars_data)
-    printing_data('Маленькие бары:', smallest_bars)
-    print('\n')
-    closest_bars = get_closest_bar(bars_data, input_longitude, input_latitude)
-    printing_data('Близкие бары:', closest_bars)
+    for num in range(len(bars_data)):
+        print("Parse the " + str(num) + " json\n")
+        main_func(bars_data[num], input_latitude, input_longitude)
